f5b6e886c0517c42dea401bda734362085e21d64
Mohamed Aly <88594415+moAlaaeldin@users.noreply.github.com>
2022-05-16 09:14:40 +0200

    Authenticate

--- src/main/java/com/sde/passwordmanager/myDB.java
+++ src/main/java/com/sde/passwordmanager/myDB.java
@@ -10,9 +10,9 @@ public class myDB{
 
     public static void main(String[] args) throws SQLException{
         ConnectDb();
-        //Authinticate("mmmk@gmailcom","12345678");         Wrong credentials, should return 0 or -1
-        //Authinticate("mmmk@gmail.com","12345678");        Correct credentials, should return userID>0
-
+        //Authinticate("lll@hotmail.com","256488Lk");         //Wrong credentials, should return 0 or -1
+        //Authinticate("mmmk@gmail.com","12345678");        //Correct credentials, should return userID>0
+        stmt.close();
     }
 
     //Connect to DB
@@ -38,6 +38,7 @@ public class myDB{
             throw new RuntimeException(exception);
         }
     }
+
     public static int Authinticate(String username, String password) throws SQLException {
         int validity = 0;
         String queryUn = "SELECT Username, Password, UserID FROM Users WHERE Username = '" + username + "';";
@@ -47,22 +48,19 @@ public class myDB{
                 while (rs.next()) {
                     System.out.println(rs.getString("Username"));
                     if (rs.getString("Password").equals(password)) {
-                        System.out.println(5);
                         //validity is userID for correct credentials
                         validity = rs.getInt("UserID");
                     } else {
                         //Wrong Password
                         validity = -1;
                     }
-                    System.out.println(rs.getString("Password"));
+                   // System.out.println(rs.getString("Password"));
                 }
             }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        stmt.close();
-
         return validity;
     }
 
